92aa69a82fb097ef3f737700b6e38c35316f2e20
Ed-Marcavage <edmarcavage256@proton.me>
2022-11-03 22:10:27 -0400

    Remove Auth

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,15 +1,4 @@
 export type AmplifyDependentResourcesAttributes = {
-    "auth": {
-        "amplifychakraui4c4c3a16": {
-            "IdentityPoolId": "string",
-            "IdentityPoolName": "string",
-            "UserPoolId": "string",
-            "UserPoolArn": "string",
-            "UserPoolName": "string",
-            "AppClientIDWeb": "string",
-            "AppClientID": "string"
-        }
-    },
     "api": {
         "amplifychakraui": {
             "GraphQLAPIKeyOutput": "string",
